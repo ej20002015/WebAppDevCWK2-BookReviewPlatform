9390d795587cf5d7758a7197a7d6c9d784fadfe3
Evan James <sc18ej@leeds.ac.uk>
2019-12-16 01:56:17 +0000

    added backend for user reading a book and initialised page for adding a book to user's library - need to finish adding a book

--- UI/venv/app/routes.py
+++ UI/venv/app/routes.py
@@ -1,11 +1,12 @@
 from app import app, forms
-from flask import session, redirect, url_for, render_template
+from flask import session, redirect, url_for, render_template, jsonify
 import requests
+from requests.auth import HTTPBasicAuth
 
 applicationLayerDomain = "http://localhost:5001/"
 
 def checkLoggedIn():
-  return "id" in session
+  return "user" in session
 
 @app.route('/')
 def index():
@@ -26,7 +27,7 @@ def login():
     password = form.password.data
     response = requests.post(applicationLayerDomain + "Sessions", json={"username": username, "password": password})
     if response:
-      session["id"] = response.json()["id"]
+      session["user"] = response.json()
       return redirect(url_for("index"))
     else:
       return render_template("login.html", form=form, validationError=response.json()["error"])
@@ -45,9 +46,29 @@ def register():
     password = form.password.data
     response = requests.post(applicationLayerDomain + "Users", json={"username": username, "password": password})
     if response:
-      session["id"] = response.json()["id"]
+      session["user"] = response.json()
       return redirect(url_for("index"))
     else:
       return render_template("register.html", form=form, validationError=response.json()["error"])
 
   return render_template("register.html", form=form, validationError=None)
+
+@app.route("/newBook", methods=["GET"])
+def newBook():
+  if not checkLoggedIn():
+    return redirect(url_for("login"))
+
+  return render_template("newBook.html")
+
+@app.route("/test")
+def test():
+  if not checkLoggedIn():
+    return redirect(url_for("login"))
+
+  #data = {"userId": "74c15a761f9811ea99ff5800e3e11c5d", "bookId": "9d4c5e6c-1f9a-11ea-99ff-5800e3e11c5d"}
+  response = requests.get(applicationLayerDomain + "UserReadBooks/acfaa3a81fa611ea99ff5800e3e11c5d", auth=HTTPBasicAuth(session["user"]["username"], session["user"]["password"]))
+  print(response)
+  if response:
+    return jsonify(response.json())
+  else:
+    return response.json()["error"]
