f858d6eaae6a5415c8b6ae4bd893e3c730adfa99
Evan James <sc18ej@leeds.ac.uk>
2019-12-17 22:56:11 +0000

    Added thoughts to books in a users library

--- UI/venv/app/static/index.js
+++ UI/venv/app/static/index.js
@@ -47,6 +47,7 @@ $(document).ready(function() {
         type: "PUT",
         data: JSON.stringify({"favourite": false}),
         contentType: "application/json; charset=utf-8",
+        dataType: "json",
         headers: {
           "Authorization": "Basic " + btoa(username + ":" + password)
         },
@@ -63,5 +64,54 @@ $(document).ready(function() {
         }
       });
     }
+  });
+
+  $("#books").on("click", ".thoughtsButton", function() {
+    var buttonClicked = $(this);
+    var cardBody = buttonClicked.prev();
+    var thoughts = $("#thoughts" + buttonClicked.val());
+    var currentThoughts = thoughts.text() == "No thoughts - add some now" ? "" : thoughts.text();
+    thoughts.remove();
+    buttonClicked.remove();
+    cardBody.append(
+      `<div class="form-group">
+        <textarea class="form-control" rows="5" value="` + currentThoughts + `" id="thoughtsInput` + buttonClicked.val() + `"></textarea>
+      </div>
+      <button value="` + buttonClicked.val() + `" class="btn btn-primary changeThoughtsButton">Change thoughts</button>`
+    );
+  });
+
+  $("#books").on("click", ".changeThoughtsButton", function() {
+    var buttonClicked = $(this);
+    var inputBox = $("#thoughtsInput" + buttonClicked.val());
+    var newThoughts = inputBox.val();
+    $.ajax({
+      url: endpoint + "UserReadBooks/" + buttonClicked.val(),
+      type: "PUT",
+      data: JSON.stringify({"thoughts": newThoughts}),
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      headers: {
+        "Authorization": "Basic " + btoa(username + ":" + password)
+      },
+      success: function(response) {
+        var card = inputBox.parent().parent().parent();
+        inputBox.parent().remove();
+        inputBox.remove();
+        buttonClicked.remove();
+        card.html(`
+          <div class="card-body">
+            <h5 class="card-title">Your thoughts:</h5>
+            <p class="card-text" id="thoughts`+ buttonClicked.val() + `">` + newThoughts + `</p>
+          </div>
+          <button value="` + buttonClicked.val() + `" class="btn btn-primary thoughtsButton">Change your thoughts</button>
+        `);
+      },
+      error: function(response) {
+        console.log(response.error);
+        buttonClicked.parent().append(`<p class="card-text error" id="feedback">ERROR: ` + response.responseJSON.error + `</p>`)
+        buttonClicked.prop("disabled", true);
+      }
+    });
   });
 });
